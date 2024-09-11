410010444968b05fdd8c779826d78e52a5a88290
Uchiborii <zuumo219@gmail.com>
2024-09-11 17:05:46 +0900

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,11 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+		public function create()
+    {
+			//ログインしているユーザー情報をViewに渡す
+			$user = \Auth::user();
+			return view('create', compact('user'));
+    }
 }
